1525aa69241780c6f74dc20de9f042a5b6615bdd
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-06 14:48:06 -0300

    Exercício de objeto

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -59,3 +59,36 @@ let livros = [
 
 console.log(livros[1].titulo); // O Hobbit
 console.log(livros[0].autor); // Nick Mason
+
+console.log("-------------");
+
+/* Exercícios
+1) Crie um objeto chamado "pedido" contendo as
+seguintes informações:
+
+-Código (invente uma combinação de letras e números)
+-Lista de 3 produtos (basta o nome de cada um)
+-Data do pedido (coloque a data de hoje)
+-Preço
+-Prazo de entrega em dias  */
+let pedido = {
+    codigo: "123abc",
+    produtos: ["TV", "Notebook", "Geladeira"],
+    data: "06/04/2023",
+    preco: 4000.95, 
+    prazo: 7
+};
+
+/* 2) Mostre no console uma frase contendo algumas
+das informações do pedido. Exemplo:
+
+"Referente ao pedido 123xyz, os produtos foram comprados
+em 06/04/2023 custando 4500. O prazo de entrega é de
+7 dias úteis." */
+console.log(`Referente ao pedido ${pedido.codigo}, os produtos foram comprados em ${pedido.data} custando ${pedido.preco}. O prazo de entrega é de ${pedido.prazo} dias úteis.`);
+
+
+
+
+
+
