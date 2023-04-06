a974fe8a4e2469397aaef3e272f74babb3e0c2bb
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-06 16:25:08 -0300

    Condicional Encadeada

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -14,7 +14,7 @@ Operadores relacionais:
 // Exemplos
 const aluno = "Chaves";
 let nota1 = 10;
-let nota2 = 5;
+let nota2 = 9;
 let faltas = 11;
 let media = (nota1 + nota2) / 2;
 
@@ -32,8 +32,42 @@ Se a média obtida pelo aluno(a) for maior ou igual
 à média mínima da escola E se suas faltas forem abaixo
 do limite, então ele estará aprovado. Caso contrário,
 ele estará reprovado. */
+
+/* Condicional Composta */
 if (media >= mediaMinima && faltas < limiteDeFaltas) {
     console.log("aprovado");
 } else {
     console.log("reprovado");
 }
+
+/* Condicional Encadeada */
+
+/* Algoritmo para análise de desempenho
+Média menor que 3? PÉSSIMO
+Média menor que 5? RUIM
+Média menor que 7? REGULAR
+Média menor que 9? BOM
+Nenhuma das anteriores? ÓTIMO */
+
+let desempenho;
+if(media < 3){
+    desempenho = "PÉSSIMO";
+} else if(media < 5) {
+    desempenho = "RUIM";
+} else if(media < 7) {
+    desempenho = "REGULAR";
+} else if(media < 9) {
+    desempenho = "BOM";
+} else {
+    desempenho = "ÓTIMO";
+}
+
+console.log(desempenho);
+
+
+
+
+
+
+
+
