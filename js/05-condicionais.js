79c3086ccfc3b053722f0ff93f09822a3d102ec5
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-06 16:41:22 -0300

    Operador ternário

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -64,6 +64,37 @@ if(media < 3){
 
 console.log(desempenho);
 
+console.log("-----");
+
+/* if/else de uma linha (shorthand if/else) */
+let idade = 5;
+
+// Versão "normal"
+// let resultado;
+// if( idade >= 18 ){
+//     resultado = "maior"
+// } else {
+//     resultado = "menor";
+// }
+
+// Versão abreviada usando operador ternário ?:
+let resultado = idade >= 18 ? "maior" : "menor";
+
+console.log(`A pessoa é ${resultado} de idade`);
+
+console.log("-------");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
