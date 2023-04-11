2df68df4ea08f17092fb9e0c39429695e3d5b265
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-11 16:18:52 -0300

    Exercício 01 com refatoração

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -3,10 +3,23 @@ let nome = prompt("Informe seu nome: ");
 let idade = parseInt(prompt("Informe sua idade: "));
 
 // LÓGICA DAS FAIXAS DE IDADE
+
+// VERSÃO 1 (sem refatoração)
+// if ( idade >= 18 && idade < 60 ) {
+//     alert(`${nome}, você é uma pessoa adulta`);
+// } else if( idade >= 60 ){
+//     alert(`${nome}, você é uma pessoa idosa`);
+// } else {
+//     alert(`${nome}, você é uma pessoa menor de idade`);
+// }
+
+// Versão 2: com refatoração (melhorias)
+let mensagem;
 if ( idade >= 18 && idade < 60 ) {
-    alert(`${nome}, você é uma pessoa adulta`);
+    mensagem = "adulta";
 } else if( idade >= 60 ){
-    alert(`${nome}, você é uma pessoa idosa`);
+    mensagem = "idosa";
 } else {
-    alert(`${nome}, você é uma pessoa menor de idade`);
+    mensagem = "menor de idade";
 }
+alert(`${nome}, você é uma pessoa ${mensagem}`);
