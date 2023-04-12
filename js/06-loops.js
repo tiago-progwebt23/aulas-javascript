ebefc769042c80832a0bc9980a6cc77b81b26c4c
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-12 14:18:12 -0300

    Loops for/of e for/in

--- js/06-loops.js
+++ js/06-loops.js
@@ -2,7 +2,7 @@
 de repetição) 
 
 Tradicionais: while, do/while, for
-Específicos: for/of, for/in */
+Específicos: for/of (ARRAYS), for/in (OBJETOS) */
 
 // Exemplo 1: while (enquanto)
 // Este loop SOMENTE funcionará se
@@ -44,6 +44,45 @@ const alunos = [
 // Guardando o tamanho/comprimento do array
 let quantidade = alunos.length; 
 
+console.log("-- Resultado usando for --");
 for( let i = 0; i < quantidade; i++ ){
     console.log(`Nome: ${alunos[i]}`);
 }
+
+console.log("----------");
+
+/* for/of   para de/do */
+console.log("-- Resultado usando for/of --");
+
+/* for/of é uma sintaxe exclusiva (e mais simples)
+para manipulação de dados de um array */
+for( let aluno of alunos ){
+    console.log(`Nome: ${aluno}`);
+}
+
+console.log("-------");
+
+/* for/in: sintaxe exclusiva para manipulação
+de dados de um objeto */
+let pessoa = {
+    nome: "Jon Oliva",
+    idade: 66,
+    cidade: "São Paulo"
+};
+
+for( let dados in pessoa ){
+    
+    // Exibe somente o nome das props.
+    console.log(dados);
+
+    // Exibe os dados de cada prop.
+    console.log(pessoa[dados]);
+
+    console.log(
+        `${dados}: ${pessoa[dados]}`
+    );
+}
+
+
+
+
