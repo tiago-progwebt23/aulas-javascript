4e1fb2e106b721a1f1be1d5c8afde0760c078523
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-12 17:12:26 -0300

    Correção exercício 2

---
+++ js/exercicio02.js
@@ -0,0 +1,22 @@
+let valorEmDolares = 1000;
+let cotacaoDolar = 4.94;
+
+function converteMoeda(valor){
+    return valor * cotacaoDolar;
+}
+
+// Arrow Function
+// const converteMoeda = valor => valor * cotacaoDolar;
+
+let valorConvertido = converteMoeda(valorEmDolares);
+
+console.log(
+`Valor em dólares: ${valorEmDolares}`
+);
+
+console.log(
+`Valor em reais (convertido): ${valorConvertido}`
+);
+
+
+
